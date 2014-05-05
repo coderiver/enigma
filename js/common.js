81d169df5e811bf9f2378d8b6ab9bcf8016715cd
moroz <morozarturv@gmail.com>
2014-05-05 17:29:40 +0300

    index page

--- js/common.js
+++ js/common.js
@@ -1,3 +1,12 @@
 $(document).ready(function() {
-	console.log($('body').html());
+	
+// select
+var select = $('.select');
+select.find('.select__el').on('change', function() {
+  var optionSelected = $('option:selected', this),
+    valueSelected = this.value;
+  $(this).prev().find('.select__value').html(valueSelected);
+});
+
+
 });
